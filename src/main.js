0a37dd1f918c187e5fb8159760123e5e541d75b5
yuanhua <370069096@qq.com>
2019-12-26 19:52:54 +0800

    vuex试用

--- src/main.js
+++ src/main.js
@@ -1,22 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+// import store from './store'
 // 导入所有vant 所有组件
 import Vant from 'vant';
-import 'vant/lib/index.css'; 
+import 'vant/lib/index.css';
 //引入axios
-import axios from "axios";
+import axios from "axios";
 Vue.use(Vant);
 //配置服务端基础地址
-axios.defaults.baseURL="http://127.0.0.1:4000/"
+axios.defaults.baseURL = "http://127.0.0.1:4000/"
 //配置发放请求保存session信息
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true;
 //将axios注册vue
-Vue.prototype.axios = axios;
+Vue.prototype.axios = axios;
 Vue.config.productionTip = false
+// 引入vuex
+import Vuex from "vuex"
+// 注册vuex
+Vue.use(Vuex)
+var store = new Vuex.Store({
+  state: { num: 21 },
+  mutations: {
+    addNum(state) { state.num++ },
+    subNum(state) { state.num-- }
+  },
+  getters: {
+    getAge(state) { return state.num }
+  }
+})
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount('#app')
--- src/views/shopping.vue
+++ src/views/shopping.vue
@@ -12,7 +12,6 @@
     <van-cell value="再够160.1元立享'满199减20,满299减30 进入...'" is-link />
 
     <van-card
-      num="1"
       tag="满减"
       price="9.90"
       desc="规格:西瓜红"
@@ -20,12 +19,12 @@
       thumb="../assets/Cart/1.jpg"
     >
       <div slot="footer">
-        <van-button>+</van-button>
-        <van-button>-</van-button>
+        <div class="num">{{$store.getters.getAge}}</div>
+        <van-button @click="add1">+</van-button>
+        <van-button @click="sub1">-</van-button>
       </div>
     </van-card>
     <van-card
-      num="1"
       tag="满减"
       price="29.00"
       desc="规格:3盒*200支"
@@ -33,22 +32,24 @@
       thumb="../assets/Cart/2.jpg"
     >
       <div slot="footer">
-        <van-button>+</van-button>
-        <van-button>-</van-button>
+        <div class="num">{{$store.getters.getAge}}</div>
+        <van-button @click="add2">+</van-button>
+        <van-button @click="sub2">-</van-button>
       </div>
     </van-card>
     <van-cell value="再够2减立享'69元3件,89元4件 进入活..." is-link />
     <van-card
-      num="1"
       tag="满减"
       price="19.00"
       desc="颜色:指甲刀套装 薄雾蓝"
       title="指甲护理4件套"
       thumb="../assets/Cart/3.jpg"
+      class="numr"
     >
       <div slot="footer">
-        <van-button>+</van-button>
-        <van-button>-</van-button>
+        <div class="num">{{$store.getters.getAge}}</div>
+        <van-button @click="add">+</van-button>
+        <van-button @click="sub">-</van-button>
       </div>
     </van-card>
     <van-submit-bar  @submit="aaaaa" :price="3050" button-text="提交订单">
@@ -69,6 +70,24 @@ export default {
     },
     aaaaa() {
       this.$router.push({ path: "/order" });
+    },
+    add(){
+      this.$store.commit("addNum")
+    },
+    sub(){
+      this.$store.commit("subNum")
+    },
+    add1(){
+      this.$store.commit("addNum")
+    },
+    sub1(){
+      this.$store.commit("subNum")
+    },
+    add2(){
+      this.$store.commit("addNum")
+    },
+    sub2(){
+      this.$store.commit("subNum")
     }
   }
 };
@@ -80,5 +99,15 @@ export default {
 }
 .parent {
   background: #e8e8e8;
+}
+.num{
+  position:absolute;
+  font-size:1rem;
+  margin-bottom:1rem;
+  left: 21rem;
+  bottom:3.5rem
+}
+.numr{
+  position: relative;
 }
 </style>
